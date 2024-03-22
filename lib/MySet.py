cb110c1c9e86249cd372d93310787bb4c8756f30
mohamedbashir <mohaedbashir45@gmail.com>
2024-03-22 15:02:05 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self, input_list=None):
+        self.dictionary = {}
+        if input_list is not None:
+            for value in input_list:
+                self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        if value in self.dictionary:
+            del self.dictionary[value]
+        return self
+
